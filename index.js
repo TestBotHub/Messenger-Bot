99ddcafa3d2c659ca968883039c1294caa0c5cb9
hashmup <y.wizowoz@gmail.com>
2017-01-07 01:04:53 +0900

    updated the bot to speak

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/webhook/', function (req, res) {
 app.get('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging;
     for (let i=0;i<messaging_events.length;i++) {
-        let event = messaging[i];
+        let event = messaging_events[i];
         let sender = event.sender.id;
         if (event.message && event.message.text) {
             let text = event.message.text;
